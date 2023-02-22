68c39317de9d8f1f73969decdb7751a532ed6d80
davitkheoshvili <kheoshvili.dato@gmail.com>
2023-02-22 15:07:14 +0400

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react';
+import './App.css';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { WhoAmI } from './pages/WhoAmI';
+import { Portfolio } from './pages/Portfolio';
+import { Contact } from './pages/Contact';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div id='header-and-content'>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home text='hey' />} />
+          <Route path="/who-am-i" element={<WhoAmI />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
 
